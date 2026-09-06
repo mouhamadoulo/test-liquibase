0b4c28a564fa96269dae079ae7cd7aa6c9aebd81
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-09-06 14:57:57 +0200

    New column description added to Product

--- src/main/java/com/molo/test_liquibase/product/Product.java
+++ src/main/java/com/molo/test_liquibase/product/Product.java
@@ -13,6 +13,9 @@ public class Product {
     @Column(nullable = false, length = 150)
     private String name;
 
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
     protected Product() {
     }
 
@@ -30,5 +33,13 @@ public class Product {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
