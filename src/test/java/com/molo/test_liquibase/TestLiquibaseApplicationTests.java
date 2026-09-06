4fa94097318fe5af12719940ff40d79653c4a5f6
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-09-06 13:05:08 +0200

    Initial commit Test Liquibase

---
+++ src/test/java/com/molo/test_liquibase/TestLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.molo.test_liquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
