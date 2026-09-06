adc4d119f3f49c33eb4805fd94badec0fa7b28c6
Mouhamadou Moustapha LO <mouhamadoulo39@gmail.com>
2026-09-06 14:33:31 +0200

    Product Controller

---
+++ src/main/java/com/molo/test_liquibase/product/ProductController.java
@@ -0,0 +1,32 @@
+package com.molo.test_liquibase.product;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public List<Product> findAll() {
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product findById(@PathVariable Long id) {
+        return productRepository.findById(id)
+                .orElseThrow();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
